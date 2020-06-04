88dbc4769f8ad2b20e4c267019d321bed04261a2
Brandon-Harris <brandonharris177@gmail.com>
2020-06-04 11:26:35 -0600

    added player name

--- src/player.py
+++ src/player.py
@@ -1,7 +1,8 @@
 # Add capability to add Items to the player's inventory. The inventory can also be a list of items "in" the player, similar to how Items can be in a Room.
 
 class Player:
-    def __init__(self, current_room, items):
+    def __init__(self, name, current_room, items):
+        self.name = name
         self.current_room = current_room
         self.items = items
 
