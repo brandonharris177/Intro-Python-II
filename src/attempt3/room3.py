807f195f12f5264a9e9dc408bc1438749f77f83b
Brandon-Harris <brandonharris177@gmail.com>
2020-06-04 08:22:56 -0600

    Day 1 MVP met

--- src/attempt3/room3.py
+++
@@ -1,7 +0,0 @@
-class Room:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-    
-    def __repr__(self):
-        return "looking for this, {self.w_to}".format(self=self)
--- src/player.py
+++ src/player.py
@@ -1,14 +1,10 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
 class Player:
-    def __init__(self, name, current_room ):
-        self.name = name
-        self.current_room = current_room 
+    def __init__(self, current_room):
+        self.current_room = current_room
 
     def move(self, direction):
-        new_room = getattr(self.current_room, f"{direction}_to")
-
-        if new_room:
+        if hasattr(self.current_room, f"{direction}_to"):
+            new_room = getattr(self.current_room, f"{direction}_to")
             self.current_room = new_room
         else:
-            print("you cannot move that way")
+            print("cannot go that direction")
